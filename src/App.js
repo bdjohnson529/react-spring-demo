9537732032849e6295ec936182b9b3569b72c474
Ben Johnson <bdjohnson529@gmail.com>
2020-07-11 19:50:40 -0700

    rearrange

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import { useSpring } from 'react-spring';
-
-import { RotatingKeyframe } from './components/KeyframeAnimations.js';
-import { TracedSVG, AnimatedText, StaticImage }  from './components/SpringComponents.js';
+import { TracedSVG, AnimatedText, RotatingImage }  from './components/ImageViews.js';
 
 import image from './img/logo.svg';
 import './App.css';
 
 
 function App() {
-  const textAnimation = useSpring({
+
+  const fadeInAnimation = useSpring({
      from: { opacity: 0, color:"red", number:0},
      to: { opacity: 1, color:"blue", number:1}
    });
 
+  const svgSpring = useSpring({
+      from: { x: 0, opacity: 0 },
+      to: async next => {
+        await next({ x: 120, opacity: 1 })
+      },
+      delay: 3000,
+      config: { tension: 20, friction: 10 }
+  });
+
   return (
+
     <div className="App">
-
-
       <div className="App-header">
-      
-        <AnimatedText style={textAnimation} />
-
-        <img src={ image } width="25%" height="25%"/>
-
-        <StaticImage image={ image } />
-
+        <AnimatedText style={fadeInAnimation} text={"This text fades in."}/>
+        <RotatingImage hello={"hello"} image={image} />
+        <TracedSVG style={svgSpring} image={image} />
       </div>
-
 
       <div>
         <p>Page 2</p>
